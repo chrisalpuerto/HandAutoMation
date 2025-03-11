f4540ef9b12464c2a00f39993d81cbe2bca0b0e7
chris <bbhriss@gmail.com>
2025-03-11 01:33:38 -0700

    updated file

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 #chris alpuerto
-
+from pythonosc import udp_client
 import cv2
 import mediapipe as mp
 import numpy as np
 
+# OSC client
+client = udp_client.SimpleUDPClient("127.0.0.1",12345)
 
 mp_hands = mp.solutions.hands
 mp_draw = mp.solutions.drawing_utils
 hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7)
 
-frameWidth = 720
-frameHeight = 600
+frameWidth = 640
+frameHeight = 500 
+
+# distance range
+min_distance = 20
+max_distance = 370
 
 # initialize left and right hand distances
 left_distance = 0
 right_distance = 0
+normalized_distance = 0
+normailzed_distance_left = 0
+normalized_distance_right = 0
 
 cap = cv2.VideoCapture(1)
 cap.set(3, frameWidth)
@@ -46,12 +55,19 @@ while cap.isOpened():
             index_x, index_y = int(index_tip.x * w), int(index_tip.y * h)
 
             distance = np.linalg.norm(np.array([thumb_x, thumb_y]) - np.array([index_x, index_y]))
+            normalized_distance = (distance - min_distance) / (max_distance - min_distance)
+            normalized_distance = max(0, min(1, normalized_distance))
+            
             if label == "Left":
                 left_distance = int(distance)
+                normalized_distance_left = (left_distance - min_distance) / (max_distance - min_distance)
+                normalized_distance_left = max(0, min(1, normalized_distance_left))
                 text_position = (50, 50)
                 color = (0,255,0)
             else:
                 right_distance = int(distance)
+                normalized_distance_right = (right_distance - min_distance) / (max_distance - min_distance)
+                normalized_distance_right = max(0, min(1, normalized_distance_right))
                 text_position = (w - 200, 50)
                 color = (255,0,0)
 
@@ -66,10 +82,13 @@ while cap.isOpened():
             
             # distance text
             cv2.putText(frame, f"{int(distance)} px", text_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+            client.send_message("/live/track/volume", normalized_distance_left)
+            client.send_message("/live/track/reverb", normalized_distance_right)
+            
 
     cv2.imshow("HandAutoMation", frame)
 
-    print(f"left: {left_distance}, right: {right_distance}")
+    print(f"normalized: {normalized_distance} left: {right_distance}, right: {right_distance}")
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- testfunctions.py
+++ testfunctions.py
@@ -1,3 +1,8 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+from pythonosc import udp_client
+
+
+client = udp_client.SimpleUDPClient("127.0.0.1",12345)
+client.send_message("/live/track/reverb", 1)
